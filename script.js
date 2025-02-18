f425f9b43155b296ea7a88901c02696373fa89e8
Dimitar-Iliev-ops <58664953+Dimitar-Iliev-ops@users.noreply.github.com>
2025-02-18 20:47:31 +0200

    Update script.js
    ..

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", revealOnScroll);
     revealOnScroll();
 });
+
+function toggleDropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+// Close dropdown if user clicks outside
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
